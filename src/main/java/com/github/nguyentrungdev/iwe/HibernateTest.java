b7a49a574d262036692ce9e547587881650f3954
NguyenTrungKT <rickchenboss@gmail.com>
2015-01-03 20:24:16 +0700

    Update WallPost

--- src/main/java/com/github/nguyentrungdev/iwe/HibernateTest.java
+++ src/main/java/com/github/nguyentrungdev/iwe/HibernateTest.java
@@ -1,8 +1,11 @@
 package com.github.nguyentrungdev.iwe;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 
 import com.github.nguyentrungdev.iwe.pojo.User;
+import com.github.nguyentrungdev.iwe.pojo.WallPost;
 import com.github.nguyentrungdev.iwe.util.HibernateUtil;
 
 public class HibernateTest {
@@ -15,8 +18,16 @@ public class HibernateTest {
 		user.setPassWord("123456");
 		user.setIsActive(true);
 		session.save(user);
+		System.out.println(user.getId());
+		WallPost post = new WallPost();
+		post.setContent("TrungTest");
+		post.setIsActive(true);
+		post.setCreatedDate(new Date());
+		post.setUser_Id(user.getId());
+		session.save(post);
 		session.getTransaction().commit();
 		System.out.println("done");
+		
 	}
 
 }
---
+++ src/main/java/com/github/nguyentrungdev/iwe/pojo/WallPost.java
@@ -0,0 +1,59 @@
+package com.github.nguyentrungdev.iwe.pojo;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class WallPost {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@Column(columnDefinition="mediumtext")
+	private String content;
+	private Long user_Id;
+	@Column(columnDefinition="BIT", length=1)
+	private Boolean isActive;
+	private Date createdDate;
+	private Date modifiedDate;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Long getUser_Id() {
+		return user_Id;
+	}
+	public void setUser_Id(Long user_Id) {
+		this.user_Id = user_Id;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+}
